6182488985f63e32f55368f649ab88f47dd5ab21
rahayu19w <younklex50@gmail.com>
2024-04-02 07:16:19 +0800

    uji coba hitung pytagoras

---
+++ pytagoras.py
@@ -0,0 +1,18 @@
+# def main():
+#     a = float(input("Masukan Nilai a :"))
+#     b = float(input("Masukan Nilai b :"))
+#     c = (a**2 + b**2)**0.5
+#     print("nilai c adalah :", c)
+
+#     main()
+
+print('##  Program Python Menghitung Luas Pytagoras ##')
+print('===============================================')
+print()
+ 
+def hitungLuasSegitiga(a,t):
+  return round(alas**2 + tinggi**2)**0.5
+ 
+alas = float(input('Input a: '))
+tinggi = float(input('Input b: '))
+print('Luas segitiga = ',hitungLuasSegitiga(alas, tinggi))
